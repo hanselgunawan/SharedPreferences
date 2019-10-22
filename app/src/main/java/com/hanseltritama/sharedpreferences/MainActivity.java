fa829a6253a78cf4f02dd57f7664a4201cf9015c
Hansel Tritama <10084360+hanselgunawan@users.noreply.github.com>
2019-10-21 19:02:50 -0700

    added shared preferences & object serializer

--- app/src/main/java/com/hanseltritama/sharedpreferences/MainActivity.java
+++ app/src/main/java/com/hanseltritama/sharedpreferences/MainActivity.java
@@ -1,7 +1,13 @@
 package com.hanseltritama.sharedpreferences;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SharedPreferences sharedPreferences = this.getSharedPreferences("com.hanseltritama.sharedpreferences", Context.MODE_PRIVATE);
+
+        ArrayList<String> friends = new ArrayList<>();
+        friends.add("Hnasel");
+        friends.add("Huzz");
+
+        try {
+            sharedPreferences.edit().putString("friends", ObjectSerializer.serialize(friends)).apply();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        ArrayList<String> newFriends = new ArrayList<>();
+
+        try {
+            newFriends = (ArrayList<String>) ObjectSerializer.deserialize(sharedPreferences.getString("friends", ObjectSerializer.serialize(new ArrayList<String>())));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Log.i("newFriends", newFriends.toString());
+//        sharedPreferences.edit().putString("username", "hansel").apply();
+//
+//        String username = sharedPreferences.getString("username", "default");
+//
+//        Log.i("username", username);
+
     }
 }
